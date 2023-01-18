4a8926422e3c8ed93a28ccdb35bb21e619749a65
flinty1970 <68234911+flinty1970@users.noreply.github.com>
2023-01-17 22:05:03 -0500

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -5,6 +5,7 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s/strictARP: f
 kubectl create -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
 sleep 30
 kubectl create -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/metallb2.yaml
+sleep 10
 kubectl create -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/metallb.yaml
 sleep 60
 kubectl get pods --all-namespaces
