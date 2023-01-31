d1e7ade562340aa9253c57cf5bf6461dded7ae76
flinty1970 <68234911+flinty1970@users.noreply.github.com>
2023-01-31 12:31:59 -0500

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -3,7 +3,12 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/cont
 echo Getting IP of nginx ingress controller:
 kubectl -n ingress-nginx get svc ingress-nginx-controller 
 kubectl create namespace dev
-for NS in `kubectl get ns --no-headers| awk '{ print $1 }'`; do kubectl patch serviceaccount default   -p "{\"imagePullSecrets\": [{\"name\": \"image-pull-secret\"}]}" -n $NS; done
+
+for NS in `kubectl get ns --no-headers| awk '{ print $1 }'`
+do 
+  kubectl patch serviceaccount default -p "{\"imagePullSecrets\": [{\"name\": \"image-pull-secret\"}]}" -n $NS
+done
+
 kubectl apply -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/hello-app.yaml
 kubectl get deployments -n dev
 kubectl apply -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/hello-app-service.yaml 
