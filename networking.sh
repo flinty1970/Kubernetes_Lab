6d4d4170fd8451061aa2159068a6a7ad5da82261
flinty1970 <68234911+flinty1970@users.noreply.github.com>
2023-02-08 18:58:15 -0500

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -2,25 +2,47 @@
 
 echo Install Calico network
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/tigera-operator.yaml
-sleep 30
+
+loop=1
+while [ $loop -ne 0 ]
+do
+  loop=`kubectl get pods -A --no-headers | grep -v Running| grep -v coredns |sort| uniq | wc -l`
+  sleep 1
+done
+
 
 echo Install Calico network 2
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/custom-resources.yaml
 
-echo wait 2 mins....
-sleep 120
+loop=1
+while [ $loop -ne 0 ]
+do
+  loop=`kubectl get pods -A --no-headers | grep -v Running| grep -v coredns |sort| uniq | wc -l`
+  sleep 1
+done
 
 echo StaticARP
 kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s/strictARP: false/strictARP: true/" | kubectl apply -f - -n kube-system
-sleep 30
 
 echo Install MetalLB
 kubectl create -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-sleep 30
+
+loop=1
+while [ $loop -ne 0 ]
+do
+  loop=`kubectl get pods -A --no-headers | grep -v Running| grep -v coredns |sort| uniq | wc -l`
+  sleep 1
+done
 
 echo Configure MetalLB 1
 kubectl apply -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/metallb.yaml
-sleep 30
+
+loop=1
+while [ $loop -ne 0 ]
+do
+  loop=`kubectl get pods -A --no-headers | grep -v Running| grep -v coredns |sort| uniq | wc -l`
+  sleep 1
+done
 
 echo Configure MetalLB 2
 kubectl apply -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/metallb2.yaml
