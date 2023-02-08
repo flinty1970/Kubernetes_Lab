b0cbb00c50f513976b8e8c13f23f16c9b05eea90
flinty1970 <68234911+flinty1970@users.noreply.github.com>
2023-02-08 18:21:24 -0500

    Update networking.sh

--- networking.sh
+++ networking.sh
@@ -2,24 +2,25 @@
 
 echo Install Calico network
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/tigera-operator.yaml
-sleep 10
+sleep 30
 
 echo Install Calico network 2
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/custom-resources.yaml
 
-echo wait 3 mins....
-sleep 180
+echo wait 2 mins....
+sleep 120
 
 echo StaticARP
 kubectl get configmap kube-proxy -n kube-system -o yaml | sed -e "s/strictARP: false/strictARP: true/" | kubectl apply -f - -n kube-system
-sleep 20
+sleep 30
 
 echo Install MetalLB
 kubectl create -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
-sleep 60
+sleep 30
 
 echo Configure MetalLB 1
 kubectl apply -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/metallb.yaml
+sleep 30
 
 echo Configure MetalLB 2
 kubectl apply -f https://raw.githubusercontent.com/flinty1970/Kubernetes_Lab/main/metallb2.yaml
